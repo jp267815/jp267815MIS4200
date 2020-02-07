b97802db6f93f4906fef37e812a5f21b7e924845
Jaelyn Pounds <jp267815@ohio.edu>
2020-02-07 11:36:07 -0500

    Student

---
+++ Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jp267815MIS4200.Models
+{
+    public class Student
+    {
+
+        public int studentId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public string email { get; set; }
+
+        public string phone { get; set; }
+
+        public string address { get; set; }
+
+        public string city { get; set; }
+
+        public string state { get; set; }
+
+        public int zip { get; set; }
+
+        public ICollection<Grade> Grade { get; set; }
+
+
+    }
+}
