0023d9a1d56b71d2bc19cdfd89a2594ef53fded8
Jaelyn Pounds <jp267815@ohio.edu>
2020-02-24 18:43:58 -0500

    errors on border and string

--- Models/Student.cs
+++ Models/Student.cs
@@ -16,35 +16,40 @@ namespace jp267815MIS4200.Models
 
         [Display(Name = "First name")]
         [Required(ErrorMessage = "Student first name is required")]
+      [StringLength(20)]
       
         public string firstName { get; set; }
 
         [Display(Name = "Last name")]
         [Required(ErrorMessage = "Student last name is required")]
-        
+        [StringLength(20)]
+      
         public string lastName { get; set; }
 
         [Display (Name = "Most used email address")]
         [Required(ErrorMessage = "Must enter an email address")]
+       
         public string email { get; set; }
 
         [Display(Name = "Mobile phone number")]
         [DataType(DataType.PhoneNumber)]
-        [RegularExpression(@"^(\(\d{3}\) |\d{3}-)\d{3} - \d{4}$")]
-        [Required(ErrorMessage = "Phone numbers must be in the format (xxx) xxxx - xxx or xxx-xxx-xxxx ")]
+        [RegularExpression(@"^(\(\d{3}\) |\d{3}-)\d{3} - \d{4}$",
+        ErrorMessage = "Phone numbers must be in the format (xxx) xxx - xxxx")]
         public string phone { get; set; }
 
         
         [Required(ErrorMessage = "Must enter an address")]
+        [StringLength(100)]
         public string address { get; set; }
         
 
         
         [Required(ErrorMessage = "Must enter a city")]
+        [StringLength(50)]
         public string city { get; set; }
 
         [Required(ErrorMessage = "State must be two characters")]
-        
+        [StringLength(2, MinimumLength =2 )]
         public string state { get; set; }
 
       
